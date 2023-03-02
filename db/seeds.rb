3605b623f4ee57e298f55e48a32998981c0f45a8
Stiflerzak <hilarymwendia97@gmail.com>
2023-03-02 22:54:41 +0300

    Seeded data to categories table

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+puts "🌱 Seeding data..."
+
+# place your seeds here
+
+Category.create(name: "Freelance")
+Category.create(name: "Proffesional")
+Category.create(name: "Lowcome")
+
+puts "🌱 Done seeding!"
