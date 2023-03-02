3933839f63443e96c4edc42115d956d458b240a2
Stiflerzak <hilarymwendia97@gmail.com>
2023-03-03 00:24:56 +0300

    Added category endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < Sinatra::Base
+
+    set :default_content_type, "application/json"
+
+
+
     get '/' do # this is the root route of the application (the homepage) but you can have as many routes as you want
         {hello: "Just a starting code 😃"}.to_json
     end
+
+
+get '/categories' do
+    Category.all.to_json
 end
+
+end
+
+
